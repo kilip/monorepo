e1ba7d8b073e43d8881f8cf0002858f4b23a4a7a
Anthonius Munthi <me@itstoni.com>
2018-11-09 00:38:03 +0800

    fixed coding standard

--- src/Application.php
+++ src/Application.php
@@ -26,7 +26,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Application extends BaseApplication
 {
-    public const VERSION = '1.0-dev';
+    const VERSION = '1.0-dev';
 
     public function __construct()
     {
