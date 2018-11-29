0235e83944b37b9877c265285528347c554ad666
Anthonius Munthi <me@itstoni.com>
2018-11-29 11:03:50 +0800

    test 3

--- etc/travis/deploy.sh
+++ etc/travis/deploy.sh
@@ -5,6 +5,7 @@ set -e
 SF_PATH=/home/frs/project/monorepo
 RELEASE_PATH=${TRAVIS_BUILD_DIR}/build/release
 EXIT=0
+MESSAGE= "Deploy success!"
 
 # exec command only when $EXIT=0
 function doExec()
@@ -21,9 +22,7 @@ mkdir -pv ${RELEASE_PATH} | echo "directory ${RELEASE_PATH} exists"
 doExec "./bin/monorepo compile --ansi -vvv ${TRAVIS_BUILD_DIR}/build/release" 1
 doExec "rsync -r --delete-after --quiet ${TRAVIS_BUILD_DIR}/build/release ${SF_USER}:${SF_PASS}@${SF_HOST}:${SF_PATH}" 2
 
-if [[ 0 == ${EXIT} ]]; then
-    echo "Deploy success!";
-elif [[ 1 == $EXIT ]]; then
+if [[ 1 == $EXIT ]]; then
     echo "Compile failed!";
 elif [[ 2 == $EXIT ]]; then
     echo "Deploy Failed";
